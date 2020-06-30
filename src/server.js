2a1cceb84d68c413076111a91dbf45d806a41a13
Sebastian <sebastiansadowski95@interia.pl>
2020-06-30 13:42:33 +0200

    Heroku deploy

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/teai5-weather'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/teai5-weather/index.html'));
 });
